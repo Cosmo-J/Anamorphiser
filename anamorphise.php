0dda9a67c5a40b9206c703c59dacd68fd08b73e1
Cosmo-J <cosmojack@gmail.com>
2023-02-28 21:33:58 +0000

    bug fixes

--- anamorphise.php
+++ anamorphise.php
@@ -64,21 +64,40 @@ function EmptyWorkspace()
 {
     $files = glob('workspace/in_frames/*'); // get all file names
     foreach($files as $file){ // iterate files
-      if(is_file($file)) {
-        unlink($file); // delete file
-      }
+        $fileType = strtolower(pathinfo($file,PATHINFO_EXTENSION));
+        if(is_file($file) && $fileType!="txt")
+        {
+            unlink($file); // delete file
+        }
     }
     $files = glob('workspace/out_frames/*'); // get all file names
     foreach($files as $file){ // iterate files
-      if(is_file($file)) {
-        unlink($file); // delete file
-      }
+
+        $fileType = strtolower(pathinfo($file,PATHINFO_EXTENSION));
+        if(is_file($file) && $fileType!="txt") 
+        {
+            unlink($file); // delete file
+        }
     }
-    $files = glob('workspace/out_frames/*'); // get all file names
-    foreach($files as $file){ // iterate files
-      if(is_file($file)) {
-        unlink($file); // delete file
-      }
+    $files = glob('workspace/*'); // get all file names
+    foreach($files as $file)
+    { // iterate files
+        $fileType = strtolower(pathinfo($file,PATHINFO_EXTENSION));
+        if(is_file($file) && $fileType!="txt")
+        {
+            unlink($file); // delete file
+        }
+    }
+    $files = glob('uploads/*'); // get all file names
+    foreach($files as $file)
+    { // iterate files
+        $fileType = strtolower(pathinfo($file,PATHINFO_EXTENSION));
+        if(is_file($file) && $fileType!="txt")
+        {
+            unlink($file); // delete file
+        }
     }
 }
+
+EmptyWorkspace();
 ?>
